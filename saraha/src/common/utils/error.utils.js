a81f206f604d8550eab3d787e91478ab78dea1e4
Habiba <habibamo942@gmail.com>
2026-03-08 21:56:33 +0200

    errorUpdate

--- saraha/src/common/utils/error.utils.js
+++ saraha/src/common/utils/error.utils.js
@@ -1,17 +1,44 @@
-export const ConflictException = ({ message }) => {
-    const error = new Error(message);
-    error.status = 409;
-    return error;
-};
+import { NODE_ENV } from "../../../../config/config.service.js"
 
-export const NotFoundException = ({ message }) => {
-    const error = new Error(message);
-    error.status = 404;
-    return error;
-};
-export const BadRequestException = ({ message, cause }) => {
-    const error = new Error(message);
-    error.status = 400; 
-    error.cause = cause;
-    return error;
+// general customized error method
+export const ErrorResponse = ({ message = "Error", status = 400, extra = undefined } = {}) => {
+    throw new Error(message, { cause: { status, extra } })
+}
+
+// error-templates
+
+export const BadRequestException = ({ message = "BadRequestException", extra = undefined } = {}) => {
+    return ErrorResponse({ message, status: 400, extra })
+}
+
+export const ConflictException = ({ message = "ConflictException", extra = undefined } = {}) => {
+    return ErrorResponse({ message, status: 409, extra })
+}
+
+export const UnauthorizedException = ({ message = "UnauthorizedException", extra = undefined } = {}) => {
+    return ErrorResponse({ message, status: 401, extra })
+}
+
+export const NotFoundException = ({ message = "NotFoundException", extra = undefined } = {}) => {
+    return ErrorResponse({ message, status: 404, extra })
+}
+
+export const ForbiddenException = ({ message = "ForbiddenException", extra = undefined } = {}) => {
+    return ErrorResponse({ message, status: 403, extra })
+}
+
+
+// Fixed error structure
+export const globalErrorHandling = (error, req, res, next) => {
+    const status = error.cause?.status ?? 500;
+    const mood = NODE_ENV === "production";
+    const defaultErrorMessage = "something went wrong Sever error";
+    const displayErrorMessage = error.message || defaultErrorMessage;
+    
+    return res.status(status).json({
+        message: displayErrorMessage,
+        stack: mood ? undefined : error.stack,
+        status,
+        extra: mood ? status === 500 ? defaultErrorMessage : displayErrorMessage : displayErrorMessage
+    });
 };
